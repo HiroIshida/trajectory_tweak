734f64140258b50f6ec2d22d52f863c700e14228
HiroIshida <spitfire.docomo@gmail.com>
2020-03-12 06:37:45 +0900

    add tf stuff

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import copy
+import numpy as np
+import tf
+
+
 def pr2_joint_names():
     pr2_joint_name_lst = [
             "torso_lift_joint",
@@ -18,3 +23,44 @@ def pr2_joint_names():
             "head_pan_joint",
             "head_tilt_joint"]
     return pr2_joint_name_lst
+
+def qv_mult(q1, v1_):
+    length = np.linalg.norm(v1_)
+    v1 = v1_/length
+    v1 = tf.transformations.unit_vector(v1)
+    q2 = list(v1)
+    q2.append(0.0)
+    v_converted = tf.transformations.quaternion_multiply(
+        tf.transformations.quaternion_multiply(q1, q2), 
+        tf.transformations.quaternion_conjugate(q1)
+    )[:3]
+    return v_converted * length
+
+def convert(tf_12, tf_23):
+    tran_12, rot_12 = [np.array(e) for e in tf_12]
+    tran_23, rot_23 = [np.array(e) for e in tf_23]
+
+    rot_13 = tf.transformations.quaternion_multiply(rot_12, rot_23)
+    tran_13 = tran_12 + qv_mult(rot_12, tran_23)
+    return list(tran_13), list(rot_13)
+
+def normalize(vec):
+    return vec/np.linalg.norm(vec)
+
+def invert_tf(tf_inp):
+    trans, rot = tf_inp
+    rot_ = copy.copy(rot)
+    for i in range(3):
+        rot_[i] *= -1
+    rot_ = normalize(rot_)
+    trans_ = qv_mult(rot_, [-e for e in trans])
+    return [trans_, rot_]
+
+if __name__=='__main__':
+    trans = [0.0513, 0, 0.025]
+    rot = [-0.5, 0.4999999999, -0.5, 0.5]
+    tf0 = [trans, rot]
+    tf1 = invert_tf(tf0)
+    tf2 = convert(tf0, tf1)
+
+
