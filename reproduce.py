dbe8adb3beede0189d5406f471ffd85ddd14c15a
HiroIshida <spitfire.docomo@gmail.com>
2020-03-13 11:36:38 +0900

    succeed reproduce

--- reproduce.py
+++ reproduce.py
@@ -24,7 +24,9 @@ def handle_tweak(req):
 
     time.sleep(1)
     T_B_to_I = listener.lookupTransform('/base_footprint', obj_frame, rospy.Time(0))
-    T_Gtweaked_to_B_seq = tweak.simple_tweak_rule(data['tfs_r'], 0.0)
+    T_Gtweaked_to_B_seq = tweak.no_tweak_rule(data['tfs_r'], 0.0)
+    print(T_B_to_I)
+    print(T_Gtweaked_to_B_seq)
     T_Gtweaked_to_I_seq = [
             utils.convert(T_Gt_to_B, T_B_to_I) for 
             T_Gt_to_B in T_Gtweaked_to_B_seq]
@@ -35,43 +37,6 @@ def handle_tweak(req):
             }
     return JsonStringResponse(message=json.dumps(data_res))
 
-def pub_trajecgtory(T_G2B_seq, T_B2O):
-    convert = lambda T_G2B: utils.convert(T_G2B, T_B2O)
-    T_G2O_seq = map(convert, T_G2B_seq)
-    msg = PoseArray()
-
-
-
-def __handle_tweak(req):
-    def read_json():
-        with open("./test.traj", 'r') as f:
-            data = json.load(f)
-
-        obj_frame= str(data['wrt'])
-
-        T_rt_to_base_seq = []
-        T_lt_to_base_seq = []
-
-        time.sleep(1)
-        T_obj_to_base = listener.lookupTransform('/base_footprint', obj_frame, rospy.Time(0))
-
-        for i in range(data['n']):
-            T_rt_to_obj = data['tfs_r'][i] # r tool
-            T_lt_to_obj = data['tfs_l'][i] # l tool
-            T_rt_to_base = utils.convert(T_rt_to_obj, T_obj_to_base)
-            T_lt_to_base = utils.convert(T_lt_to_obj, T_obj_to_base)
-
-            T_rt_to_base_seq.append(T_rt_to_base)
-            T_lt_to_base_seq.append(T_lt_to_base)
-
-        data_res = {
-                'T_rt_seq': T_rt_to_base_seq, 
-                'T_lt_seq': T_lt_to_base_seq,
-                'av_seq': data['avs']
-                }
-        return data_res
-    data_res = read_json()
-    return JsonStringResponse(message=json.dumps(data_res))
 
 def make_pose_msg(pose):
     trans = pose[0]
@@ -81,9 +46,7 @@ def make_pose_msg(pose):
     msg = Pose(position=point_msg, orientation=quaternion_msg)
     return msg
 
-#rospy.spin()
-#srv_get_tweak.shutdown()
-if __name__ == '__main__':
+def pub_trajectory():
     with open("./test.traj", 'r') as f:
         data = json.load(f)
 
@@ -107,8 +70,12 @@ if __name__ == '__main__':
         header.frame_id = '/base_footprint'
         pose_msg.header = header
         pub.publish(pose_msg)
-
     sub = rospy.Subscriber('/joint_states', JointState, cb)
+
+#rospy.spin()
+#srv_get_tweak.shutdown()
+if __name__ == '__main__':
+    rospy.Service('get_tweak', JsonString, handle_tweak)
     rospy.spin()
 
 
