5f6fd17b13d311ab82f0c7be4b24dc1b3597e2f1
HiroIshida <spitfire.docomo@gmail.com>
2020-03-13 09:11:00 +0900

    publish posearray

--- reproduce.py
+++ reproduce.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import utils
 from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import PoseArray, Pose, Point, Quaternion
+from sensor_msgs.msg import JointState
 import tf
 import json
 import rospy
@@ -33,6 +35,13 @@ def handle_tweak(req):
             }
     return JsonStringResponse(message=json.dumps(data_res))
 
+def pub_trajecgtory(T_G2B_seq, T_B2O):
+    convert = lambda T_G2B: utils.convert(T_G2B, T_B2O)
+    T_G2O_seq = map(convert, T_G2B_seq)
+    msg = PoseArray()
+
+
+
 def __handle_tweak(req):
     def read_json():
         with open("./test.traj", 'r') as f:
@@ -64,12 +73,43 @@ def __handle_tweak(req):
     data_res = read_json()
     return JsonStringResponse(message=json.dumps(data_res))
 
-def keep_publishing():
-    pass
+def make_pose_msg(pose):
+    trans = pose[0]
+    rot = pose[1]
+    point_msg = Point(x=trans[0], y=trans[1], z=trans[2])
+    quaternion_msg = Quaternion(x=rot[0], y=rot[1], z=rot[2], w=rot[3])
+    msg = Pose(position=point_msg, orientation=quaternion_msg)
+    return msg
 
-srv_get_tweak = rospy.Service("get_tweak", JsonString, handle_tweak)
-print("start")
-rospy.spin()
+#rospy.spin()
 #srv_get_tweak.shutdown()
+if __name__ == '__main__':
+    with open("./test.traj", 'r') as f:
+        data = json.load(f)
+
+    obj_frame= str(data['wrt'])
+
+    T_rt_to_base_seq = []
+    T_lt_to_base_seq = []
+
+    time.sleep(1)
+    T_B2O = listener.lookupTransform('/base_footprint', obj_frame, rospy.Time(0))
+
+    T_G2B_seq = data['tfs_r']
+    convert = lambda T_G2B: utils.convert(T_G2B, T_B2O)
+    T_G2O_seq = map(convert, T_G2B_seq)
+    pose_list = map(make_pose_msg, T_G2O_seq)
+    pose_msg = PoseArray(poses=pose_list)
+
+    pub = rospy.Publisher('wrt', PoseArray, queue_size=2)
+    def cb(msg):
+        header = copy.copy(msg.header)
+        header.frame_id = '/base_footprint'
+        pose_msg.header = header
+        pub.publish(pose_msg)
+
+    sub = rospy.Subscriber('/joint_states', JointState, cb)
+    rospy.spin()
+
 
 
